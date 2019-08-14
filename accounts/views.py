36f25a2151500d887e6653fa8e42f75a67d64d33
Saadat Esmaeily <saadat.esmaeily@gmail.com>
2019-08-14 20:35:48 +0430

    Minor Updates Changed main folder Fixed home redirect issue after registeration

--- accounts/views.py
+++ accounts/views.py
@@ -29,7 +29,7 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(reverse('accounts:home'))
+            return redirect(reverse('home:home'))
     else:
         form = RegistrationForm()
 
